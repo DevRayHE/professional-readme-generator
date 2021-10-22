63bd15645026e6e5394e29902a4abf739dc466df
Ray He <devrayhe@gmail.com>
2021-10-22 22:29:17 +1100

    Icon and output
    Fixed up link and Icon issue for each section
    Separated output data to smaller chunk of code

--- index.js
+++ index.js
@@ -30,34 +30,56 @@ inquirer
 
     console.log(data);
 
-    const dataToWrite = 
-    `# 💻 Project Title
+    const toWriteProjectTitle = 
+`# 💻 Project Title
 
 ## ${projectTitle}
+<br/>
 
-## 📖 Description
+`
+    const toWriteDescription = 
+`## 📖 Description
 
 ### ${projectDescription}
 
-## Table of Contents
+<br/>
 
-- [Features](#features)
-- [Usage](#usage)
-- [Installation](#installation)
-- [Contribue](#contribute)
-- [Credits](#credits)
-- [License](#license)
+`
 
-## 🎇 
-## Features
+    const toWriteTOC = 
+`## Table of Contents
 
-## 🖼️
-## Usage with function video
+- [Features](#🎇features)
+- [Usage](#🖼️usage)
+- [Installation](#📓installation)
+- [Contribute](#🏗️contribute)
+- [Credits](#🏆credits)
+- [License](#📝license)
 
-## 📓
-## Installation
+<br/>
+
+`
+
+    const toWriteFeatures = 
+`## 🎇Features
+
+<br/>
+
+`
+
+    const toWriteUsage = 
+`## 🖼️ Usage
+
+<br/>
+
+`
+
+    const toWriteInstallation = 
+`## 📓 Installation
 
 [Repo url](${repoUrl})
+
+<br/>
 
 ### Follow the steps below to install:
 
@@ -76,19 +98,103 @@ inquirer
 
 7. A professional readme is now generated for your project!
 
-## 🏗️
-## Contribute
+<br/>
+
+`
+
+    const toWriteCCL =
+`## 🏗️ Contribute
 
 [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/version/2/1/code_of_conduct/code_of_conduct.md)
 
-## 🏆
-## Credits
+<br/>
 
+## 🏆 Credits
 
-## 📝
-## License
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+<br/>
+
+## 📝 License
+[https://choosealicense.com/](https://choosealicense.com/).
 `
+
+    const dataToWrite = toWriteProjectTitle 
+      + toWriteDescription
+      + toWriteTOC
+      + toWriteFeatures
+      + toWriteUsage
+      + toWriteInstallation
+      + toWriteCCL
+
+//     const dataToWrite = 
+//     `# 💻 Project Title
+
+// ## ${projectTitle}
+
+// ## 📖 Description
+
+// <br/>
+
+// ### ${projectDescription}
+
+// <br/>
+
+// ## Table of Contents
+
+// - [Features](#🎇features)
+// - [Usage](#🖼️usage)
+// - [Installation](#📓installation)
+// - [Contribue](#🏗️contribute)
+// - [Credits](#🏆credits)
+// - [License](#📝license)
+
+// <br/>
+
+// ## 🎇Features
+
+// <br/>
+
+// ## 🖼️ Usage
+
+// <br/>
+
+// ## 📓 Installation
+
+// [Repo url](${repoUrl})
+
+// <br/>
+
+// ### Follow the steps below to install:
+
+// 1. Clone the github to your local storage.
+
+// 2. Start up your favourite terminal, follow this guide here if you're not sure how to:
+// [A Quick Guide to Using Command Line - Terminal](https://towardsdatascience.com/a-quick-guide-to-using-command-line-terminal-96815b97b955).
+
+// 3. Navigate to the repository local folder.
+
+// 4. Run npm i
+
+// 5. Run node index.js
+
+// 6. Simply follow the prompt.
+
+// 7. A professional readme is now generated for your project!
+
+// <br/>
+
+// ## 🏗️ Contribute
+
+// [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/version/2/1/code_of_conduct/code_of_conduct.md)
+
+// <br/>
+
+// ## 🏆 Credits
+
+// <br/>
+
+// ## 📝 License
+// [https://choosealicense.com/](https://choosealicense.com/).
+// `
 
     fs.writeFile('README.md', dataToWrite, (error) =>
       error ? console.error(error) : console.log(`Professional Readme file - 'README.md' successfully generated!!`)
