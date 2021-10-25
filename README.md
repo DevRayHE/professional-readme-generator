713053be08044dcfd16bdbbc83c007aa51ece0f7
Ray He <devrayhe@gmail.com>
2021-10-25 17:46:38 +1100

    Updated License
    License section updated:
    Selection from List
    Badge displayed on top of Readme
    Chosen license detail displayed as per user selection

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
 # 💻Project Title
 
-## a
+## Readme
 <br/>
 
 ## 📖Description
 
-### b
+### Readme Generator display
 
 ### Langugaes used: 
 ![Markdown](https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white)
 
 ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
 
 
 
@@ -29,16 +33,18 @@
 
 ## Features🎇
 
-* 
+* featuer a
 <br/>
 
 ## Usage🖼️
+
+### Here is a video to demonstrate 
 
 <br/>
 
 ## Installation📓
 
-[Repo url]()
+[Repo url](Test url)
 
 <br/>
 
@@ -69,33 +75,21 @@
 
 ## Credits🏆
 
-### lleryayo
-[Link to this credit: ](https://github.com/Ileriayo/markdown-badges.git)
+### Credit A
+[Link to this credit: ](credit a link)
 
-### credit b
-[Link to this credit: ](https://github.com/DevRayHE/professional-readme-generator.git)
 
 <br/>
 ## License📝
 
-### MIT License
+### The Unlicense
 
-Copyright (c) [2021] [ray]
+This is free and unencumbered software released into the public domain.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to https://unlicense.org
--- index.js
+++ index.js
@@ -35,9 +35,15 @@ inquirer
       message: 'Github repo URL:',
       name: 'repoUrl',
     },
+    {
+      type: 'list',
+      message: 'Choose a license:',
+      name: 'license',
+      choices: ['BSD 2-Clause License', 'MIT License', 'The Unlicense'],
+
+    },
   ])
   .then((data) => {
-    // const {developer,projectTitle, projectDescription,repoUrl} = data;
     generalData = data;
     console.log(data);
     // start another inquirer prompt by calling askFeature funciton when promised is full-filled on general data inquirer prompt
@@ -109,8 +115,7 @@ const askCredit = () => {
       } else {
         console.log('Credits:', creditData.join(', '));
 
-        const {developer,projectTitle, projectDescription, languageUsed, repoUrl} = generalData;
-        const {creditDesc, creditLink} = creditData;
+        const {developer, projectTitle, projectDescription, languageUsed, repoUrl, license} = generalData;
 
         const languageData = [];
 
@@ -140,7 +145,9 @@ const askCredit = () => {
         }
 
         const toWriteProjectTitle = 
-`# 💻Project Title
+`<br/>
+
+# 💻Project Title
 
 ## ${projectTitle}
 <br/>
@@ -188,6 +195,8 @@ ${languageDisplay}
 
         const toWriteUsage = 
 `## Usage🖼️
+
+### Here is a video to demonstrate 
 
 <br/>
 
@@ -241,19 +250,40 @@ ${languageDisplay}
 
 `
 
-        // console.log(credit.creditDesc + credit.creditLink);
+        let toWriteLicense = '';
+        let licenseDisplay = '';
 
-//         const toWriteCredits =
-// `## Credits🏆
+        // Based on user chosen option, generate License part of the Readme
+        switch (license) {
+          case 'BSD 2-Clause License':
 
-// ![${creditDesc}](${creditLink}
+            licenseDisplay = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
 
-// <br/>
+            toWriteLicense = `
+<br/>
 
-// `
+## License📝
 
-        const toWriteLicense = 
-`<br/>
+### BSD 2-Clause License
+
+Copyright (c) ${year}, ${developer} All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`;
+
+          break;
+          case 'MIT License':
+
+            licenseDisplay = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+            toWriteLicense =`
+<br/>
+
 ## License📝
 
 ### MIT License
@@ -276,10 +306,34 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-`
+SOFTWARE.`;
 
-        const dataToWrite = toWriteProjectTitle 
+          break;
+          case 'The Unlicense':
+
+            licenseDisplay = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+
+            toWriteLicense = `
+<br/>
+
+## License📝
+
+### The Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to https://unlicense.org`
+          break;
+        }
+
+        const dataToWrite = licenseDisplay
+          + toWriteProjectTitle 
           + toWriteDescription
           + toWriteTOC
           + toWriteFeatures
@@ -288,6 +342,8 @@ SOFTWARE.
           + toWriteContribute
           + toWriteCredits
           + toWriteLicense
+          // + toWriteTests
+          // + toWriteQuestions
 
         fs.writeFile('README.md', dataToWrite, (error) =>
           error ? console.error(error) : console.log(`Professional Readme file - 'README.md' successfully generated!!`)
